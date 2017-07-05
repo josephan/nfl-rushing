7928b9a6127d0b4988f853e088f8e31534b3871b
Joseph An <an.joseph891@gmail.com>
2017-07-05 03:28:16 -0400

    Filter CSV with names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
   def filter_csv_params(params)
     {
-      yrds: params[:yrds],
-      lng: params[:lng],
-      td: params[:td],
-      format: 'csv'
+      name:    params[:name],
+      yrds:    params[:yrds],
+      lng:     params[:lng],
+      td:      params[:td],
+      format:  'csv'
     }
   end
 end
